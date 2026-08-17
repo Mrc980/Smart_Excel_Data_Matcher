43b7744cd4ed58f30d234b5a2d07afdf3b93e77f
Mrc980 <inanchy46@gmail.com>
2026-08-17 08:56:05 -0400

    Add cleaning for addresses

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import re
+import unicodedata
+
 
 def clean_text(value):
     "converts value to formatted string and detects empty values"
@@ -13,9 +16,44 @@ def clean_text(value):
 
     return value
 
-print(clean_text("  Ottawa         Public Library  "))
-print(clean_text(" "))
-print(clean_text(None))
+def clean_address(value):
+    value = clean_text(value)
+
+    if value == "":
+        return ""
+
+    value = re.sub(r"[^a-z0-9\s]", "", value)
+    value = " ".join(value.split())
+
+    replacements = {
+        "street": "st",
+        "avenue": "ave",
+        "boulevard": "blvd",
+        "road": "rd",
+        "drive": "dr",
+        "north": "n",
+        "south": "s",
+        "east": "e",
+        "west": "w",
+        "highway": "hwy",
+        "apartment": "apt",
+
+    }
+
+    words = value.split()
+
+    cleaned_words = []
+
+    for word in words:
+        if word in replacements:
+            cleaned_words.append(replacements[word])
+        else:
+            cleaned_words.append(word)
+
+    words = cleaned_words
+
+    return " ".join(cleaned_words)
+
     
     
 
